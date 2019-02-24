ecf07fceb3833d88274a3ea8e00ae3cbfdd24e49
dylan prem <dylanprem90@gmail.com>
2019-02-24 11:02:22 -0500

    dev

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -90,7 +90,6 @@ class Register extends Component {
                     value={this.state.email}
                     onChange={this.onChange}
                     error={errors.email}
-                    info="This site uses Gravatar so if you want a profile image, use a Gravatar email"
                   />
                   <TextFieldGroup
                     placeholder="Password"
